437776be175c6740a5f2dca47fdd1430d5343cfc
ubox107 <sparshp@onswitchboard.com>
2020-10-30 19:27:48 +0530

    #add a new api users, to get users list

--- app.js
+++ app.js
@@ -13,6 +13,35 @@ app.post('/', function(req, res) {
   });
 });
 
+app.get("/users", () =>{
+  const users = [
+    {
+      firstName: "Jack",
+      lastName: "Daniel",
+      userEmail: "jack@daniel.com",
+      gender: "male",
+    },
+    {
+      firstName: "John",
+      lastName: "Daniel",
+      userEmail: "john@daniel.com",
+      gender: "male",
+    },
+    {
+      firstName: "Rock",
+      lastName: "Johnson",
+      userEmail: "rock@johnson.com",
+      gender: "male",
+    },
+    {
+      firstName: "Veronica",
+      lastName: "Lincoln",
+      userEmail: "veronica@lincoln.com",
+      gender: "female",
+    }
+  ];
+  res.json(users);
+});
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
